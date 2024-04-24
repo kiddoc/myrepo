2c75ee21a2895c8993f951da972b6c6aa0ebb711
kiddoc <gdoctor.who@gmail.com>
2024-04-24 19:03:23 +0300

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,16 @@
+num=int(input("Enter number:"))
+
+
+def factorial(num):
+    
+ if num==1 or num==1:
+    return 1
+
+ else:
+    
+    return num * factorial((num)-1 )
+
+
+
+
+print (factorial(num))
